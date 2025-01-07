ffacdd9dd8ba2c89543b7208c694eb041a021b69
goldenpants1 <grog.88@mail.ru>
2025-01-07 21:03:56 +0300

    установлен дефолтный английский

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default= "chrome",
                      help="Choose browser: chrome or firefox")
 
-    parser.addoption('--language', action='store', default=None, help="Choose language: '--language=en' or '--language=ru'")
+    parser.addoption('--language', action='store', default='en', help="Choose language: '--language=en' or '--language=ru'")
 
 @pytest.fixture(scope="function")
 def browser(request):
